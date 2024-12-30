90fd64b7b502b85a3009250a1af853348ec5af7d
colobrodo <turbosmi3000@gmail.com>
2024-12-30 22:54:08 +0100

    Added adjustable camera

--- src/main.rs
+++ src/main.rs
@@ -130,6 +130,9 @@ fn cast(scene: &Scene, ray: &Ray) -> Vec3 {
 
 // TODO: add field of view to the camera
 struct Camera {
+    forward: Vec3,
+    up: Vec3,
+    right: Vec3,
     position: Vec3,
     zoom: f64,
 }
@@ -137,23 +140,42 @@ struct Camera {
 impl Camera {
     fn new(position: Vec3) -> Self {
         Self {
+            up: Vec3::y_axis(),
+            forward: Vec3::z_axis(),
+            right: Vec3::x_axis(),
             position,
             zoom: 1.0,
         }
     }
 
+    /// Creates a new camera positioned at zero looking toward the z axis.
     fn default() -> Self {
+        Self::new(Vec3::zero())
+    }
+
+    /// Creates a camera that looks at a specific point from a specific position.
+    /// The point is referred to be the center of the screen.
+    fn look_at(position: Vec3, point: Vec3) -> Self {
+        let world_up = Vec3::y_axis();
+        let forward = (point - position).normalize();
+        let right = world_up.cross(forward).normalize();
+        // to get an orthonormal base, we should calculate the up vector with two perpendicular vectors
+        let up = forward.cross(right).normalize();
         Self {
-            position: Vec3::zero(),
+            forward,
+            up,
+            right,
+            position,
             zoom: 1.0,
         }
     }
 
-    /// create a ray from the camera position to the relative uv coordinate on his screen
+    /// Create a ray from the camera position to the relative uv coordinate on his screen.
     fn shoot_to(&self, u: f64, v: f64) -> Ray {
+        let direction = self.forward * self.zoom + self.up * v + self.right * u;
         Ray {
             origin: self.position,
-            direction: Vec3::new(u, v, self.zoom) - self.position,
+            direction: direction.normalize(),
         }
     }
 }
@@ -162,7 +184,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     let args = Args::parse();
 
     // TODO: make the camera settable also from the file, with a custom orientation
-    let camera = Camera::default();
+    let camera = Camera::look_at(Vec3::new(4.0, 0.1, 0.0), Vec3::new(0.0, 0.0, 7.0));
 
     let content = fs::read_to_string(args.scene)?;
     let mut parser = SceneParser::new(&content);
